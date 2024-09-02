9085874149479747249a991c2df43bf165ef3f75
Rahul <rahul.raj@idt.net>
2024-09-02 15:32:12 +0530

    Fix an issue in FIAPRequestHandler.h import

--- packages/in_app_purchase/in_app_purchase_storekit/darwin/Classes/FIAPRequestHandler.h
+++ packages/in_app_purchase/in_app_purchase_storekit/darwin/Classes/FIAPRequestHandler.h
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <FLTRequestHandlerProtocol.h>
+#import "FLTRequestHandlerProtocol.h"
 #import <Foundation/Foundation.h>
 #import <StoreKit/StoreKit.h>
 
